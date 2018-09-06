996b9a745e670c53fad6a81879a6a94687eb6cb4
M <m-hammed.bakkouch@centraliens.net>
2018-09-06 11:32:49 +0200

    first

---
+++ app/src/main/java/fr/massy/covoit/covoit_massy/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.massy.covoit.covoit_massy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
